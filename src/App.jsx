6634ab1bef193ebe4af2b6f6567c3407660b8d75
joel dio <joelchondo@gmail.com>
2025-03-05 18:41:48 +0300

    fixing deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css';
-import Hero from './Components/Hero';
 import Footer from './Components/Footer';
 import Contact from './Components/Contact';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
+import Hero from './Components/Hero';
 
 
 function App() {
